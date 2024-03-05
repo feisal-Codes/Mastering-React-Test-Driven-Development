a1f4f87ef912abfcb4f400c844dbb9994c69c2df
feisal-Codes <feisalmib@gmail.com>
2024-03-05 17:20:34 +0300

    tests to cover display and clicking of a button

--- appointments/component/AppointmentsDayView.jsx
+++ appointments/component/AppointmentsDayView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Appointment from "./appointment";
 const appointmentTimeOfDay = (startsAt) => {
   const [h, m] = new Date(startsAt).toTimeString().split(":");
   return `${h}:${m}`;
@@ -8,10 +9,20 @@ function AppointmentsDayView({ appointments }) {
   return (
     <div id="appointmentsDayView">
       <ol>
-        {appointments.map((appointment, idx) => (
-          <li key={idx}>{appointmentTimeOfDay(appointment.startsAt)}</li>
+        {appointments.map((appointment) => (
+          <li key={appointment.startsAt}>
+            <button type="button">
+              {appointmentTimeOfDay(appointment.startsAt)}
+            </button>
+          </li>
         ))}
       </ol>
+      ;
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[0]} />
+      )}
     </div>
   );
 }
